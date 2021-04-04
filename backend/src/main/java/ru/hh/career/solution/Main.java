3a5847ce0cf13fde3874e9236acfe70ed78e6af0
Nikita Basharov <colorbash3000@gmail.com>
2021-04-04 12:00:15 +0300

    add checkstyle

--- backend/src/main/java/ru/hh/career/solution/Main.java
+++ backend/src/main/java/ru/hh/career/solution/Main.java
@@ -6,12 +6,12 @@ import ru.hh.nab.starter.NabApplication;
 
 public class Main {
 
-    public static void main(String[] args) {
-        NabApplication
-                .builder()
-                .configureJersey(JerseyConfig.class)
-                .bindToRoot()
-                .build()
-                .run(ProdConfig.class);
-    }
+  public static void main(String[] args) {
+    NabApplication
+        .builder()
+        .configureJersey(JerseyConfig.class)
+        .bindToRoot()
+        .build()
+        .run(ProdConfig.class);
+  }
 }
